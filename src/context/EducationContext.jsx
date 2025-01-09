11dd9d63e480822470e51a7d78c35ba120e28257
shheeeeshka <102031292+shheeeeshka@users.noreply.github.com>
2025-01-09 13:19:49 +0300

    g9434g

--- src/context/EducationContext.jsx
+++ src/context/EducationContext.jsx
@@ -24,50 +24,6 @@ export const EducationContextProvider = ({ children }) => {
                 console.error(err);
             } finally {
                 setIsEducationLoading(false);
-                setModules([
-                    {
-                        "title": "Основы SQL",
-                        "description": "Изучение баз данных.",
-                        "createdAt": "01.01.25",
-                        "previewImg": "https://static19.tgcnt.ru/posts/_0/6a/6af98dc08bc1cb1a88e340d1c32f979d.jpg",
-                    },
-                    {
-                        "title": "Продвинутый SQL",
-                        "description": "Углубленное изучение запросов.",
-                        "createdAt": "01.01.25",
-                        "previewImg": "https://static19.tgcnt.ru/posts/_0/6a/6af98dc08bc1cb1a88e340d1c32f979d.jpg",
-                    },
-                    {
-                        "title": "Оптимизация запросов",
-                        "description": "Методы повышения производительности SQL.",
-                        "createdAt": "01.01.25",
-                        "previewImg": "https://static19.tgcnt.ru/posts/_0/6a/6af98dc08bc1cb1a88e340d1c32f979d.jpg",
-                    },
-                    {
-                        "title": "Работа с транзакциями",
-                        "description": "Управление транзакциями и их свойствами.",
-                        "createdAt": "01.01.25",
-                        "previewImg": "https://static19.tgcnt.ru/posts/_0/6a/6af98dc08bc1cb1a88e340d1c32f979d.jpg",
-                    },
-                    {
-                        "title": "Безопасность баз данных",
-                        "description": "Методы защиты данных и доступа.",
-                        "createdAt": "01.01.25",
-                        "previewImg": "https://static19.tgcnt.ru/posts/_0/6a/6af98dc08bc1cb1a88e340d1c32f979d.jpg",
-                    },
-                    {
-                        "title": "SQL для аналитиков",
-                        "description": "Анализ данных с помощью SQL-запросов.",
-                        "createdAt": "01.01.25",
-                        "previewImg": "https://static19.tgcnt.ru/posts/_0/6a/6af98dc08bc1cb1a88e340d1c32f979d.jpg",
-                    },
-                    {
-                        "title": "Интеграция SQL",
-                        "description": "Связывание SQL с другими языками.",
-                        "createdAt": "01.01.25",
-                        "previewImg": "https://static19.tgcnt.ru/posts/_0/6a/6af98dc08bc1cb1a88e340d1c32f979d.jpg",
-                    },
-                ]);
             }
         };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://192.168.0.104:5070";
+// export const API_URL = "http://192.168.0.104:5070";
+export const API_URL = "https://shheeeeshka-octopus-learning-server-9ddf.twc1.net";
