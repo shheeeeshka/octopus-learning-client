313ababefccda61617a53ea6e79a50bbd9be40be
shheeeeshka <102031292+shheeeeshka@users.noreply.github.com>
2025-01-10 13:46:05 +0300

    home page styles corrected

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -42,67 +42,69 @@ const HomePage = () => {
                 </div>
                 <img src="/photo-banner-1.png" alt="right image" style={{ height: 200 }} />
             </div>
-            <div className={styles["h-p-modules-wrapper"]}>
-                <ul className={styles["h-p-modules"]}>
-                    {
-                        (!isEducationLoading && Array.isArray(filteredModules) && filteredModules?.length < 1) && <div>
-                            <h3>Урок не найден :(</h3>
-                        </div>
-                    }
-                    {
-                        isEducationLoading && <div>
-                            <h3>Подгружаем уроки для Вас</h3>
-                        </div>
-                    }
-                    {
-                        (Array.isArray(filteredModules) && filteredModules?.length > 0) && filteredModules?.map((module, ind) => (
-                            <Link to={`module?topic=${module.title}`} key={ind} className={styles["module-card"]}>
-                                <div style={{ height: 40 }}>
-                                    <div style={{ height: 60, position: "relative", width: 60 }}>
-                                        <img
-                                            src={module.previewImg} alt="preview"
-                                            style={{ borderRadius: 15, width: "100%", position: "absolute", top: "-52%" }}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="d-flex flex-column align-items-center" style={{ gap: 20, padding: "0px 25px 15px 25px", width: "100%" }}>
-                                    <div className="d-flex flex-column" style={{ width: "70%", textAlign: "center", height: "100px", gap: 20 }}>
-                                        <span style={{ fontWeight: 600, fontSize: 17, color: "black" }}>{module.title}</span>
-                                        <span style={{ fontSize: 14, color: "#dadee1" }}>{module.description}</span>
-                                    </div>
-                                    <div className="d-flex flex-column" style={{ width: "100%", fontSize: 14, gap: 10 }}>
-                                        <div className="d-flex justify-content-between" style={{ color: "black" }}>
-                                            <span>Пройдено</span>
-                                            <span>{75}%</span>
+            <div className={styles["home-page-main-container"]}>
+                <div className={styles["h-p-modules-wrapper"]}>
+                    <ul className={styles["h-p-modules"]}>
+                        {
+                            (!isEducationLoading && Array.isArray(filteredModules) && filteredModules?.length < 1) && <div>
+                                <h3>Урок не найден :(</h3>
+                            </div>
+                        }
+                        {
+                            isEducationLoading && <div>
+                                <h3>Подгружаем уроки для Вас</h3>
+                            </div>
+                        }
+                        {
+                            (Array.isArray(filteredModules) && filteredModules?.length > 0) && filteredModules?.map((module, ind) => (
+                                <Link to={`module?topic=${module.title}`} key={ind} className={styles["module-card"]}>
+                                    <div style={{ height: 40 }}>
+                                        <div style={{ height: 60, position: "relative", width: 60 }}>
+                                            <img
+                                                src={module.previewImg} alt="preview"
+                                                style={{ borderRadius: 15, width: "100%", position: "absolute", top: "-52%" }}
+                                            />
                                         </div>
-                                        <ProgressBar variant="success" now={75} style={{ height: 10 }} />
                                     </div>
-                                </div>
-                            </Link>
-                        ))
-                    }
-                </ul>
-            </div>
-            <div className={styles["home-right-sidebar"]}>
-                <div className={styles["achievements-widget"]}>
-                    <div className={styles["achievement-wrapper"]}>
-                        <div style={{ height: 75, width: 75 }}>
-                            <img src="/achievement-1.png" alt="" style={{ height: "100%" }} />
-                        </div>
-                        <span style={{ fontSize: 18, fontWeight: 600 }}>{"Ворота открыты"}</span>
-                    </div>
-                    <div className={styles["achievement-wrapper"]}>
-                        <div style={{ height: 75, width: 75 }}>
-                            <img src="/achievement-2.png" alt="" style={{ height: "100%" }} />
-                        </div>
-                        <span style={{ fontSize: 18, fontWeight: 600 }}>{"Успешный старт"}</span>
-                    </div>
+                                    <div className="d-flex flex-column align-items-center" style={{ gap: 20, padding: "0px 25px 15px 25px", width: "100%" }}>
+                                        <div className="d-flex flex-column" style={{ width: "75%", textAlign: "center", height: "100px", gap: 20 }}>
+                                            <span style={{ fontWeight: 600, fontSize: 17, color: "black" }}>{module.title}</span>
+                                            <span style={{ fontSize: 14, color: "rgb(168 168 168)" }}>{module.description}</span>
+                                        </div>
+                                        <div className="d-flex flex-column" style={{ width: "100%", fontSize: 14, gap: 10 }}>
+                                            <div className="d-flex justify-content-between" style={{ color: "black", fontSize: 16 }}>
+                                                <span>Пройдено</span>
+                                                <span>{75}%</span>
+                                            </div>
+                                            <ProgressBar variant="success" now={75} style={{ height: 10 }} />
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))
+                        }
+                    </ul>
                 </div>
-                <div className={styles["calendar-wrapper"]}>
-                    <Calendar
-                        onChange={() => handleCalendarDateChange()}
-                        value={selectedDate}
-                    />
+                <div className={styles["home-right-sidebar"]}>
+                    <div className={styles["achievements-widget"]}>
+                        <div className={styles["achievement-wrapper"]}>
+                            <div style={{ height: 75, width: 75 }}>
+                                <img src="/achievement-1.png" alt="" style={{ height: "100%" }} />
+                            </div>
+                            <span style={{ fontSize: 18, fontWeight: 600 }}>{"Ворота открыты"}</span>
+                        </div>
+                        <div className={styles["achievement-wrapper"]}>
+                            <div style={{ height: 75, width: 75 }}>
+                                <img src="/achievement-2.png" alt="" style={{ height: "100%" }} />
+                            </div>
+                            <span style={{ fontSize: 18, fontWeight: 600 }}>{"Успешный старт"}</span>
+                        </div>
+                    </div>
+                    <div className={styles["calendar-wrapper"]}>
+                        <Calendar
+                            onChange={() => handleCalendarDateChange()}
+                            value={selectedDate}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
