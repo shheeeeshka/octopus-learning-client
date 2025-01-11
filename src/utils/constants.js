ac5ff0134569a7edae2032be1d22865de4a9d4d8
shheeeeshka <102031292+shheeeeshka@users.noreply.github.com>
2025-01-11 13:00:26 +0300

    g04g04

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://192.168.1.101:5070";
-export const API_URL = "https://shheeeeshka-octopus-learning-server-f6fd.twc1.net";
+export const API_URL = "https://shheeeeshka-octopus-learning-server-699e.twc1.net";
