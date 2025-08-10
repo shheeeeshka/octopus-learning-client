582ae99309a2ea579d255541ccc5224e32143419
shheeeeshka <kuznetsovinnokenty25@gmail.com>
2025-08-10 09:50:58 -0700

    019tj20jg2

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import "react-calendar/dist/Calendar.css";
 
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/Router/AppRouter";
+import { useEffect } from "react";
+import { API_URL } from "./utils/constants";
 
 const App = () => {
+  useEffect(() => {
+    fetch(API_URL).catch(() => {});
+  }, []);
+
   return (
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
